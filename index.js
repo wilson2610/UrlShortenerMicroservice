3dbf1c5fc4c59dfa74e26d09096f6c55746695cb
Sean Wilson <seanwilson4.sw@gmail.com>
2024-03-09 23:59:58 +0000

    Initial commit

--- index.js
+++ index.js
@@ -2,13 +2,59 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let Site;
+
+const siteSchema = new mongoose.Schema({
+  original_url: String,
+  short_url: Number
+});
+
+Site = mongoose.model('Site', siteSchema);
+
+app.route('/api/shorturl').post((req, res) => {
+  console.log(req.body.url);
+  var reqUrl = req.body.url;
+
+  
+
+  Site.estimatedDocumentCount((countErr, docCount) => {
+    if (countErr) {
+      res.send('estimatedDocumentCount error');
+    }
+    
+    const site = new Site({
+      original_url: reqUrl,
+      short_url: docCount + 1
+    });
+    
+    site.save((saveErr, savedUrl) => {
+      if (saveErr) {
+        res.send('save error');
+      }
+
+      res.json({
+        original_url: savedUrl.original_url,
+        short_url: savedUrl.short_url
+      });
+    });
+  });
+});
+
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
